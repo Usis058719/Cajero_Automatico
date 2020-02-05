e9bb421a3f76aec41f8951416786bb780ed52575
Antale2000 <usis027418@ugb.edu.sv>
2020-02-05 13:41:43 -0600

    nombre de controles

---
+++ Cajero Automatico/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Cajero_Automatico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblcantidad = new System.Windows.Forms.Label();
+            this.lblrespuesta = new System.Windows.Forms.Label();
+            this.txtcantidad = new System.Windows.Forms.TextBox();
+            this.btnretirar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblcantidad
+            // 
+            this.lblcantidad.AutoSize = true;
+            this.lblcantidad.Location = new System.Drawing.Point(46, 36);
+            this.lblcantidad.Name = "lblcantidad";
+            this.lblcantidad.Size = new System.Drawing.Size(49, 13);
+            this.lblcantidad.TabIndex = 0;
+            this.lblcantidad.Text = "Cantidad";
+            // 
+            // lblrespuesta
+            // 
+            this.lblrespuesta.AutoSize = true;
+            this.lblrespuesta.Location = new System.Drawing.Point(226, 43);
+            this.lblrespuesta.Name = "lblrespuesta";
+            this.lblrespuesta.Size = new System.Drawing.Size(67, 13);
+            this.lblrespuesta.TabIndex = 1;
+            this.lblrespuesta.Text = "Respuesta ?";
+            // 
+            // txtcantidad
+            // 
+            this.txtcantidad.Location = new System.Drawing.Point(108, 36);
+            this.txtcantidad.Name = "txtcantidad";
+            this.txtcantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtcantidad.TabIndex = 2;
+            // 
+            // btnretirar
+            // 
+            this.btnretirar.Location = new System.Drawing.Point(119, 83);
+            this.btnretirar.Name = "btnretirar";
+            this.btnretirar.Size = new System.Drawing.Size(111, 67);
+            this.btnretirar.TabIndex = 3;
+            this.btnretirar.Text = "RETIRAR";
+            this.btnretirar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 259);
+            this.Controls.Add(this.btnretirar);
+            this.Controls.Add(this.txtcantidad);
+            this.Controls.Add(this.lblrespuesta);
+            this.Controls.Add(this.lblcantidad);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "cajero automatico";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblcantidad;
+        private System.Windows.Forms.Label lblrespuesta;
+        private System.Windows.Forms.TextBox txtcantidad;
+        private System.Windows.Forms.Button btnretirar;
+    }
+}
+
